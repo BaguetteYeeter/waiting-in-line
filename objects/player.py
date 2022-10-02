5991fff35ccb5a0d92334c8b584290e55e3a94f8
BaguetteYeeter <baguetteyeeter@gmail.com>
2022-10-03 00:19:42 +0100

    DVD SCREENSAVER

--- objects/player.py
+++ objects/player.py
@@ -1,20 +1,31 @@
 import pygame               #add the pygame library
 from config import *        #add config.py
 config = read_config()      #read the config
+import random
 
 class Player(pygame.sprite.Sprite):     #make a player thats a sprite
     def __init__(self):                 #make __init__ (used for objects)
         super().__init__()              #i dont know
-        self.image = pygame.Surface((100, 100))              #make a blank surface with 100x100
-        self.rect = pygame.Rect((50, 50), (100, 100))        #make a rectangle at 50,50 with 100x100
+        self.image = pygame.Surface((50, 50))              #make a blank surface with 100x100
+        self.rect = pygame.Rect((0, 0), (50, 50))        #make a rectangle at 50,50 with 100x100
         pygame.draw.rect(self.image,(0, 50, 250), self.rect) #draw the rectangle on the surface (how did this even get into prod)
+        self.velocity = [3, 3]
     def move(self, keys):                        #make a move function
-        speed = int(300/config["screen"]["fps"]) #speed is 300/fps because i said so
-        if keys[pygame.K_a]:                     #if a is pressed
-            self.rect.x -= speed                 #go left by speed
-        if keys[pygame.K_d]:                     #if d is pressed
-            self.rect.x += speed                 #go right by speed
-        if keys[pygame.K_w]:                     #if w is pressed
-            self.rect.y -= speed                 #go up
-        if keys[pygame.K_s]:                     #if s is pressed
-            self.rect.y += speed                 #go down
+        speed = 5
+        self.rect.x += int(self.velocity[0])
+        self.rect.y += int(self.velocity[1])
+        if self.rect.x <= 0:
+            self.velocity[0] = 0-(self.velocity[0])
+        if self.rect.y <= 0:
+            self.velocity[1] = 0-(self.velocity[1])
+        if self.rect.x >= config["screen"]["resolution"][0]-50:
+            self.velocity[0] = 0-(self.velocity[0])
+        if self.rect.y >= config["screen"]["resolution"][1]-50:
+            self.velocity[1] = 0-(self.velocity[1])
+        if (
+            (self.rect.x == 0 and self.rect.y == 0) or
+            (self.rect.x == 0 and self.rect.y >= config["screen"]["resolution"][1]-50) or
+            (self.rect.x >= config["screen"]["resolution"][0]-50 and self.rect.y == 0) or
+            (self.rect.x >= config["screen"]["resolution"][0]-50 and self.rect.y >= config["screen"]["resolution"][1]-50)
+        ):
+            print("CORNER!!!!!!!!!!")
--- screen.py
+++ screen.py
@@ -1,8 +1,10 @@
 import pygame #add the pygame library
+from config import *
+config = read_config()
 
 class screen:                                            #make a class called screen
     def pygame_init():                                   #make a function called pygame_init
         global mainScreen                                #make the screen accessible everywhere
         pygame.init()                                    #initialize pygame
-        mainScreen = pygame.display.set_mode((500, 500)) #make the screen with resolution 500x500, TODO: use config
+        mainScreen = pygame.display.set_mode(config["screen"]["resolution"]) #make the screen with resolution 500x500, TODO: use config
         return mainScreen                                #return the mainScreen variable
